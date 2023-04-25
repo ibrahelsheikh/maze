390e3864909a7b8512e16d1b0c9c1be5dedc33c4
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2023-04-25 16:49:25 +0200

    Add files via upload

--- Constants.py
+++ Constants.py
@@ -4,21 +4,15 @@ RED_COLOR_RGB = (255, 0, 0)
 GREEN_COLOR_RGB = (0, 255, 0)
 BLUE_COLOR_RGB = (0, 0, 255)
 WIDTH = 500
-NUM_ROWS = 10
+NUM_ROWS = 5
 NUM_EPISODES = 1000
 EPSILON = 3  # Integer 1-10. 2 = 20% random, 3 = 30% random ...
 CELL_SIZE = WIDTH / NUM_ROWS
 ACTIONS = ['left', 'right', 'up', 'down']
-TERMINAL_CELLS = [(4, 5), (7, 4) ,(9, 9)]
-CELL_VALUES = [((2, 0), -10),
-               ((2, 1), -10),
-               ((2, 2), -10),
-               ((2, 3), -10),
-               ((2, 4), -10),
-               ((2, 5), -10),
-               ((2, 6), -10),
-               ((2, 7), -10),
-               ((2, 8), -10),
-               ((4, 5), 50),
-               ((7, 4), 30),
-                ((9, 9), 100)]
+TERMINAL_CELLS = [(4, 4), (2, 2)]
+CELL_VALUES = [((1, 0), -10),
+               ((1, 1), -10),
+               ((1, 2), -10),
+               ((1, 3), -10),
+               ((4, 4), 30),
+               ((2, 2), 50)]
